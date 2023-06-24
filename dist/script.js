a4c8c812d2f2718320260326cf499f9beaa36973
Martin Prikasky <prikasky.martin@gmail.com>
2023-06-24 11:16:14 +0200

    fix script.js - slides order

--- dist/script.js
+++ dist/script.js
@@ -1,48 +1,36 @@
-// Select all slides
 const slides = document.querySelectorAll(".slide");
 
-// loop through slides and set each slides translateX property to index * 100% 
 slides.forEach((slide, indx) => {
   slide.style.transform = `translateX(${(indx * 105)-((Math.floor(slides.length/2))*100)}%)`;
 });
-// current slide counter
+
 let curSlide = Math.floor(slides.length/2);
 
-// select next slide button
 const nextSlide = document.querySelector(".btn-next");
 
-
-// maximum number of slides
 let maxSlide = slides.length - 1;
 
-// add event listener and navigation functionality
 nextSlide.addEventListener("click", function () {
-  // check if current slide is the last and reset current slide
   if (curSlide === maxSlide) {
     curSlide = 0;
   } else {
     curSlide++;
   }
 
-//   move slide by -100%
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${105 * (indx - curSlide)}%)`;
   });
 });
 
-// select prev slide button
 const prevSlide = document.querySelector(".btn-prev");
 
-// add event listener and navigation functionality
 prevSlide.addEventListener("click", function () {
-  // check if current slide is the first and reset current slide to last
   if (curSlide === 0) {
     curSlide = maxSlide;
   } else {
     curSlide--;
   }
 
-  //   move slide by 100%
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${10 * (indx - curSlide)}%)`;
   });
